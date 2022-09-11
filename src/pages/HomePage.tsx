e01f0e239599637d4643ee1f76c941d1bb95b1f1
VladPashkovsky <v.v.pashkovsky@gmail.com>
2022-09-11 20:09:23 +0300

    search

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { githubApi } from '../store/github/github.api'
+import { useDebounce } from '../hooks/useDebounce'
+import { util } from 'prettier'
 
 const HomePage = () => {
-  const {data, isLoading, isError} = githubApi.useSearchUsersQuery('Vlad')
+
+  const [search, setSearch] = useState('')
+  const [dropdown, setDropdown] = useState(false)
+  const debounced = useDebounce(search)
+  const { data, isLoading, isError } = githubApi.useSearchUsersQuery(debounced,
+    { skip: debounced.length < 3, refetchOnFocus: true })
+
+  useEffect(() => {
+    setDropdown(debounced.length > 3 && data?.length! > 0)
+  }, [debounced, data])
 
   return (
-    <div>
-      
+    <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
+      {isError && <p className='text-center text-red-600'>Error...</p>}
+      <div className='relative w-[560px]'>
+        <input
+          type='text'
+          className='border py-2 px-4 w-full h-[42px] mb-2'
+          placeholder='Github username search...'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        {dropdown && <ul
+          className='List-one absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white'>
+          {isLoading && <p className='text-center'>Loading...</p>}
+          {data?.map(user =>
+            <li
+              key={user.id}
+              className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
+            >
+              {user.login}</li>)}
+        </ul>}
+      </div>
+
     </div>
   )
 }
--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -4,10 +4,11 @@ import { IUser, ServerResponse } from '../../models/models'
 export const githubApi = createApi({
   reducerPath: 'github/api',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.github.com' }),
+  refetchOnFocus: true,
   endpoints: (build) => ({
     searchUsers: build.query<IUser[], string>({
       query: (search: string) => ({ url: 'search/users', params: { q: search, per_page: 10 } }),
-      transformResponse: (response: ServerResponse<IUser>) => response.items
+      transformResponse: (response: ServerResponse<IUser>) => response.items,
     }),
   }),
 })
